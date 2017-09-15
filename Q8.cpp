e145303f4f5db187d302cda64b61d23cada88c69
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-09-15 20:03:06 +0530

    Create Q8.cpp

---
+++ Q8.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void arm(int n)
+{
+  int i=1,s=0;
+  while(i<=n)
+  {
+    int m=i,k=0;
+    while(m)
+    {
+        m/=10;
+        k++;
+    }
+    m=i;
+    s=0;
+    while(m)
+    {
+      s+=pow(m%10,k);
+      m/=10;
+     }
+    if(s==i) cout<<s<<" is Armstrong";
+    i++;
+   }
+}
+int main()
+{
+  int n;
+  cin>>n;
+  srm(n);
+  return 2;
+}
