a82d6766a0f330d3b831ad14074904da7ef7ac10
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-09-15 19:57:40 +0530

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void prime(int m)
+{
+  int n=1;
+  while(n<=m)
+  {
+    int i=1,s=0;
+    while(i<=n)
+    {
+     if(!(n%i)) s++;
+      i++;
+    }
+    if(s==2) cout<<j;
+   }
+}
+int main()
+{
+  int a;
+  cin>>a;
+  prime(a);
+  return 1;
+}
